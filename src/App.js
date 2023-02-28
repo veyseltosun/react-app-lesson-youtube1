fa4d2a1ebd523797c5df25482ac80648c58d37cd
veyseltosun <postalarim1923@gmail.com>
2023-03-01 01:46:19 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -4,35 +4,62 @@ import { useState } from 'react';
 
 function App() {
   const [isim, setIsim] = useState("");
-  const [errorMessage , setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [entered, setEntered] = useState(false);
+
 
   const handleChange = (event) => {
     setIsim(event.target.value);
   }
   const handleSubmit = (isim) => {
 
-    isim.length < 6 ? setErrorMessage("Kullanıcı ismi en az 6 karater olmalırdır.") : 
-   setErrorMessage("");
+    if (isim.length < 6) {
+      setErrorMessage("Kullanıcı ismi en az 6 karater olmalırdır.");
+
+    }
+    else {
+
+      setErrorMessage("");
+      setEntered(true)
+    }
+
+    setIsim("")
+
   };
 
   return (
     <div className="App">
       <h1>React Dersleri</h1>
-      <input
-      name='isim'
-      placeholder=" Name ...  "
-      value={isim}
-      onChange={ handleChange}
-      />
+      {entered ? (
 
-      <button onClick={ () => handleSubmit(isim)}>Giriş</button>
-      <br/>
-      { errorMessage ? <h3>{errorMessage}</h3> : <h2>Giriş yapınız ... </h2>}
-      
-      
-      
+        <>
+          <h2>Welcome !!!</h2>
+          <button onClick={() => setEntered(false)}> Çıkış Yap</button>
+        </>
 
-      
+
+      ) : (
+        <>
+
+          <input
+            name='isim'
+            placeholder=" Name ...  "
+            value={isim}
+            onChange={handleChange}
+          />
+
+          <button onClick={() => handleSubmit(isim)}>Giriş</button>
+          <br />
+          {errorMessage ? <h3>{errorMessage}</h3> : <h2>Giriş yapınız ... </h2>}
+
+        </>
+      )}
+
+
+
+
+
+
     </div>
   );
 }
